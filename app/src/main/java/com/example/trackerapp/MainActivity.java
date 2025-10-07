5309000aaa3130c8420ec362c84f162842002957
elijkon <timetocleanup20@gmail.com>
2025-10-07 12:11:07 -0500

    Turning in

--- app/src/main/java/com/example/trackerapp/MainActivity.java
+++ app/src/main/java/com/example/trackerapp/MainActivity.java
@@ -7,14 +7,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentManager fg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            //colorsModel = new ViewModelProvider(this).get(ColorsViewModel.class);
+            fg = getSupportFragmentManager();
+            FragmentTransaction trans = fg.beginTransaction();
+
+            InputFragment cf = new InputFragment();
+            trans.add(R.id.InputFragment, cf, "InputFrag");
+
+            BottomFragment cl = new BottomFragment();
+            trans.add(R.id.BottomFragment, cl, "BottomFrag");
+
+
+            trans.commit();
+        }
+
+
+
+
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
